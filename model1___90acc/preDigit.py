ebca7e331d465c274584d6babe56f34ff7b6c65e
Nitin Verma <phnx4um@gmail.com>
2019-04-18 05:36:46 -0700

    First model 90% Accuracy

---
+++ model1___90acc/preDigit.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras.models import Sequential, model_from_json
+from keras.layers import Conv2D, MaxPool2D, Dropout, Dense, Flatten
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import RMSprop
+from keras.callbacks import ReduceLROnPlateau
+import os
+
+def loadModel():
+    json_file = open('model.json', 'r')
+    model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(model_json)
+    model.load_weights("mnist_model.h5")
+    return model
+
+
+
+model = loadModel()
+optimizer = RMSprop(lr=0.001)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+#print(model.summary())
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+y_test = to_categorical(y_test, num_classes=10)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+
+X_test = X_test.astype('float32')
+X_test /= 255
+
+
+
+loss_and_metrics = model.evaluate(X_test, y_test, verbose=2)
+
+print("Test Loss", loss_and_metrics[0])
+print("Test Accuracy", loss_and_metrics[1])
+
+
+img = X_test[390]
+img = img.reshape(-1,28,28,1)
+img_class = model.predict_classes(img)
+prediction = img_class[0]
+classname = img_class[0]
+print("Class: ",classname)
+
+
+img = img.reshape((28,28))
+plt.imshow(img)
+plt.title(classname)
+plt.show()
+
+
+
+
+
